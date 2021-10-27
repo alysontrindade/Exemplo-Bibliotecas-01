1d6e55743c893e78d0ee14e7a95b6b7f48ddfacc
AlysonTrindade <42615041+alysontrindade@users.noreply.github.com>
2021-10-27 14:11:00 +0000

    Initial commit

---
+++ calculadora.py
@@ -0,0 +1,9 @@
+def soma(a, b):
+    s = a+b
+
+    return s
+
+def subtracao(a, b):
+    s = a-b
+
+    return s
---
+++ main.py
@@ -0,0 +1,17 @@
+import math as mt
+# from math import sin, log, tan
+import calculadora as calc
+from calculadora import subtracao as sub
+
+import math1
+
+
+print(mt.sin(90))
+print(mt.sin(180))
+print(mt.log(2,3))
+print(mt.tan(45))
+print()
+print(calc.soma(9, 7))
+print(sub(10,4))
+
+print(math1.)
